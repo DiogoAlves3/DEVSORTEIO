e2fb7dd97cd3687ac658bec9376f95edf20ffece
DiogoAlves <diogoalvesdev@gmail.com>
2024-05-19 01:29:25 -0300

    Devsorteio

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ function generatenumber(){
     const min = Math.ceil(document.querySelector(".min").value)
     const max = Math.floor(document.querySelector(".max").value)
 
-    const result =  Math.floor(Math.random() * (max - min + 1)) + min;
+    if(min >= max){
+        alert("O VALOR MINIMO NAO PODE SER MAIOR OU IGUAL QUE O VALOR MAXIMO")
+    } else{
+        const result =  Math.floor(Math.random() * (max - min + 1)) + min;
 
     alert(result)
+    }
 }
 
 
